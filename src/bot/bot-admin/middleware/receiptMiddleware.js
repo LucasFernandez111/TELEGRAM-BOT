74a2e6ec47f69e7ffb9b4fb6709becc43e689035
Lucas <flucasyoel@gmail.com>
2024-06-10 21:03:25 -0300

    fix(bot-admin): :bug: Fix  file_id undefined

--- src/bot/bot-admin/middleware/receiptMiddleware.js
+++ src/bot/bot-admin/middleware/receiptMiddleware.js
@@ -1,5 +1,5 @@
 const receiptMiddleware = (ctx) => {
-  if (!ctx.update.message.document.file_id)
+  if (!ctx.update.message.document)
     throw Error("Envia el archivo como un documento");
 
   return;
