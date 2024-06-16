a5dbd0d21f41d618366afcd16bf2227f0d443e8d
Lucas <flucasyoel@gmail.com>
2024-06-16 01:18:40 -0300

    feat(bot): :sparkles: Prevent messages while bot is inactive

--- src/index.js
+++ src/index.js
@@ -8,17 +8,23 @@ const app = express();
 
 app.listen(PORT, () => {
   botAdmin
-    .launch(() => console.log("Bot Admin activado"))
+    .launch({ dropPendingUpdates: true }, () =>
+      console.log("Bot Admin activado")
+    )
     .catch((error) => {
       console.log("El bot admin fallo: ", error);
     });
   botClient
-    .launch(() => console.log("Bot Client activado"))
+    .launch({ dropPendingUpdates: true }, () =>
+      console.log("Bot Client activado")
+    )
     .catch((error) => {
       console.log("El bot cliente fallo: ", error);
     });
   botGroup
-    .launch(() => console.log("Bot Group activado"))
+    .launch({ dropPendingUpdates: true }, () =>
+      console.log("Bot Group activado")
+    )
     .catch((error) => {
       console.log("El bot grupo fallo: ", error);
     });
